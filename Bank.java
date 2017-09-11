58fd2bdd7bb0a0c1d134beb9d4a9241e61e9631c
AndyLu-2017 <31222383+AndyLu-2017@users.noreply.github.com>
2017-09-10 23:35:57 -1000

    Add files via upload

--- Bank.java
+++ Bank.java
@@ -3,7 +3,7 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 /**
- * TempConvert
+ * Bank
  * This program demonstrates the use of a GridBaglayout manager
  * to create a simple GUI interface involving labels, textfields, 
  * and buttons.
@@ -12,37 +12,32 @@ import java.util.*;
  */
 @SuppressWarnings("serial")
 public class Bank extends JFrame {
-	/*
-	 *Define the GUI components as class variables
-	 *which are accessible from anywhere.
-	 *
-	 */
 	
-	
-	JLabel balance = new JLabel();
-	JLabel month = new JLabel();
-	
-	JButton buttonA = new JButton();
-	JButton button1 = new JButton();
-	JButton button2 = new JButton();
-	JButton button3 = new JButton();
-	JButton button4 = new JButton();
-	
-	int accountNum = 1;
-	Account[] account = {
-			new SavingAccount(50), 
-			new SavingAccount(250), 
-			new CheckingAccount(100)
+	static int accountNum = 1;
+	static Account[] account = {
+			new SavingAccount(101, 50), 
+			new SavingAccount(102, 250), 
+			new CheckingAccount(103, 100)
 	};
-	String[] accountChoice = { "01", "02", "03"};
-	@SuppressWarnings("rawtypes")
-	JComboBox accountSelect = new JComboBox<Object>(accountChoice);
-	ArrayList<Account> accountList = new ArrayList<Account>();
+	static String[] accountChoice = { 
+			Integer.toString(account[0].getAccountNum()), 
+			Integer.toString(account[1].getAccountNum()), 
+			Integer.toString(account[2].getAccountNum())
+	};
+	static String[] accountType = {"Saving Account", "Checking Account"};
+	
+	static JComboBox<String> accountSelect = new JComboBox<String>(accountChoice);
+	//ArrayList<Account> accountList = new ArrayList<Account>();
+	
+	static JLabel balance = new JLabel();
+	static JLabel month = new JLabel();
+	
 	/*
 	 *Initialize the frame and display it.
 	 */
 	public Bank() {
-		setTitle("Bank Accounts Management Menu");
+		setTitle("Virtual ATM");
+		setLocationRelativeTo(null);
 		//define the layout manager
 		getContentPane().setLayout(new GridBagLayout());
 		GridBagConstraints grid = new GridBagConstraints();
@@ -60,20 +55,20 @@ public class Bank extends JFrame {
 		//(1, 0)
 		accountSelect.addActionListener(new ActionListener() {
 			public void actionPerformed (ActionEvent e) {
-				jBActionPerformed(e);
-				updateLabel();
+				accountNum = accountSelect.getSelectedIndex() + 1;
+				update();
 			}
 		});
 		addItem(accountSelect, 1, 0, 2, 1, GridBagConstraints.NORTH, 
 				GridBagConstraints.HORIZONTAL);
 		
 		//(3, 0)
-		buttonA.setText("+");
+		JButton buttonA = new JButton("+");
 		//specify the action listener for the button
 		buttonA.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				bAActionPerformed(e);
-				updateLabel();
+				createAccountMenu();
+				update();
 			}
 		});
 		grid.gridx = 3;
@@ -87,7 +82,7 @@ public class Bank extends JFrame {
 		getContentPane().add(label2, grid);
 		
 		//(1, 1)
-		updateLabel();
+		update();
 		addItem(balance, 1, 1, 2, 1, GridBagConstraints.NORTH, 
 				GridBagConstraints.HORIZONTAL);
 		
@@ -97,12 +92,13 @@ public class Bank extends JFrame {
 		getContentPane().add(month, grid);
 		
 		//(0, 2)
-		button1.setText("Deposit");
+		JButton button1 = new JButton("Deposit");
 		//specify the action listener for the button
 		button1.addActionListener(new ActionListener() {
 			public void actionPerformed (ActionEvent e) {
-				b1ActionPerformed(e);
-				updateLabel();
+				deposit(accountNum, Double.parseDouble(
+						JOptionPane.showInputDialog(null, "Deposit Amount:")));
+				update();
 			}
 		});
 		grid.gridx = 0;
@@ -110,12 +106,13 @@ public class Bank extends JFrame {
 		getContentPane().add(button1, grid);
 		
 		//(1, 2)
-		button2.setText("Withdrawl");
+		JButton button2 = new JButton("Withdrawl");
 		//specify the action listener for the button
 		button2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				b2ActionPerformed(e);
-				updateLabel();
+				withdrawl(accountNum, Double.parseDouble(
+						JOptionPane.showInputDialog(null, "Withdrawl Amount:")));
+				update();
 			}
 		});
 		grid.gridx = 1;
@@ -123,12 +120,12 @@ public class Bank extends JFrame {
 		getContentPane().add(button2, grid);
 		
 		//(2, 2)
-		button3.setText("MonthEnd");
+		JButton button3 = new JButton("MonthEnd");
 		//specify the action listener for the button
 		button3.addActionListener(new ActionListener() {
 			public void actionPerformed (ActionEvent e) {
-				b3ActionPerformed(e);
-				updateLabel();
+				monthEnd();
+				update();
 			}
 		});
 		grid.gridx = 2;
@@ -136,11 +133,11 @@ public class Bank extends JFrame {
 		getContentPane().add(button3, grid);
 		
 		//(3, 2)
-		button4.setText("Quit");
+		JButton button4 = new JButton("Quit");
 		//specify the action listener for the button
 		button4.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				b4ActionPerformed(e);
+				System.exit(0);
 			}
 		});
 		grid.gridx = 3;
@@ -153,45 +150,95 @@ public class Bank extends JFrame {
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 	}
 	
-	//defines the action listener for the accountSelect
-	private void jBActionPerformed(ActionEvent e) {
-		accountNum = accountSelect.getSelectedIndex() + 1;
-	}
-	
-	//defines the action listener for the buttonA
-	private void bAActionPerformed(ActionEvent e) {
+	// WIP, incomplete
+	private static void createAccountMenu() {
+		JFrame CAMenu = new JFrame();
+		CAMenu.setTitle("Create New Account");
+		CAMenu.setLocationRelativeTo(null);
+		//define the layout manager
+		CAMenu.getContentPane().setLayout(new GridBagLayout());
+		GridBagConstraints grid = new GridBagConstraints();
+		grid.ipadx = 4;
+		grid.ipady = 4;
+		grid.insets = new Insets(4, 4, 4, 4);
+		grid.fill = GridBagConstraints.BOTH;
 		
+		//(0, 0)
+		JLabel label1 = new JLabel("Account Type:");
+		grid.gridx = 0;
+		grid.gridy = 0;
+		CAMenu.getContentPane().add(label1, grid);
+		
+		//(1, 0)
+		JComboBox<String> jBox1 = new JComboBox<String>(accountType);
+		grid.gridx = 1;
+		grid.gridy = 0;
+		CAMenu.getContentPane().add(jBox1, grid);
+				
+		//(0, 1)
+		JLabel label2 = new JLabel("Account Number:");
+		grid.gridx = 0;
+		grid.gridy = 1;
+		CAMenu.getContentPane().add(label2, grid);
+		
+		//(1, 1)
+		TextField textf2 = new TextField();
+		textf2.setColumns(30);
+		grid.gridx = 1;
+		grid.gridy = 1;
+		CAMenu.getContentPane().add(textf2, grid);
+		
+		//(0, 2)
+		JLabel label3 = new JLabel("Initial Deposit:");
+		grid.gridx = 0;
+		grid.gridy = 2;
+		CAMenu.getContentPane().add(label3, grid);
+		
+		//(1, 2)
+		TextField textf3 = new TextField();
+		grid.gridx = 1;
+		grid.gridy = 2;
+		CAMenu.getContentPane().add(textf3, grid);
+		
+		//(0, 3)
+		JButton buttonCancel = new JButton("Cancel");
+		buttonCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				CAMenu.dispose();
+			}
+		});
+		grid.gridx = 0;
+		grid.gridy = 3;
+		CAMenu.getContentPane().add(buttonCancel, grid);
+		
+		//(1, 3)
+		JButton buttonConfirm = new JButton("Confirm");
+		buttonConfirm.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String accType = accountType[jBox1.getSelectedIndex()];
+				int accountNUm = Integer.parseInt(textf2.getText());
+				double initAmount = Double.parseDouble(textf3.getText());
+				addAccount(accType, accountNUm, initAmount);
+				CAMenu.dispose();
+			}
+		});
+		grid.gridx = 1;
+		grid.gridy = 3;
+		grid.anchor = GridBagConstraints.EAST;
+		grid.fill = GridBagConstraints.VERTICAL;
+		CAMenu.getContentPane().add(buttonConfirm, grid);
+		
+		CAMenu.pack();
+		CAMenu.setVisible(true);
 	}
 	
-	//defines the action listener for the button1
-	private void b1ActionPerformed(ActionEvent e) {
-		deposit(account, accountNum, Double.parseDouble(
-				JOptionPane.showInputDialog(null, "Deposit Amount:")));
-	}
-	
-	//defines the action listener for the button2
-	private void b2ActionPerformed(ActionEvent e) {
-		withdrawl(account, accountNum, Double.parseDouble(
-				JOptionPane.showInputDialog(null, "Withdrawl Amount:")));
-	}
-	//defines the action listener for the button3
-	private void b3ActionPerformed(ActionEvent e) {
-		monthEnd(account);
-	}
-	
-	//defines the action listener for the button4
-	private void b4ActionPerformed(ActionEvent e) {
-		//System.out.println("Exited from Bank Accounts Management Menu.");
-		System.exit(0);
-	}
-	
-	private void updateLabel() {
+	private static void update() {
 		balance.setText("$" + account[accountNum-1].getBalance());
 		month.setText("Month - " + Account.getMonthNum());
 	}
 	
 	private static void deposit
-	(Account[] account, int accountNum, double amount) {
+	(int accountNum, double amount) {
 		account[accountNum-1].Deposit(amount);
 		String string = "Account" + accountNum + " +$" + amount + 
 				"\nNew Balance: $" + account[accountNum-1].getBalance();
@@ -199,14 +246,14 @@ public class Bank extends JFrame {
 	}
 	
 	private static void withdrawl
-	(Account[] account, int accountNum, double amount) {
+	(int accountNum, double amount) {
 		account[accountNum-1].Withdrawl(amount);
 		String string = "Account" + accountNum + " -$" + amount + 
 				"\nNew Balance: $" + account[accountNum-1].getBalance();
 		JOptionPane.showMessageDialog(null, string);
 	}
 	
-	private static void monthEnd(Account[] account) {
+	private static void monthEnd() {
 		String string = "End of Month" + Account.getMonthNum() + " Account Summary:";
 		for(int i = 0; i < account.length; i++) {
 			string += "\n   - Account" + (i+1) + 
@@ -229,6 +276,20 @@ public class Bank extends JFrame {
 		gc.anchor = align;
 		gc.fill = fill;
 		getContentPane().add(c, gc);
+	}
+	
+	private static void addAccount(String accountType, 
+			int accountNum, double initialDeposit) {
+		account = Arrays.copyOf(account, account.length + 1);
+		if(accountType.contains("saving")) 
+			account[account.length - 1] = new SavingAccount(accountNum, initialDeposit);
+		else if(accountType.contains("checking")) 
+			account[account.length - 1] = new CheckingAccount(accountNum, initialDeposit);
+		else account[account.length - 1] = new SavingAccount(accountNum, initialDeposit);
+		
+		accountChoice = Arrays.copyOf(accountChoice, accountChoice.length + 1);
+		accountChoice[accountChoice.length - 1] = Integer.toString(accountNum);
+		accountSelect.addItem( Integer.toString(accountNum));
 	}
 	
 	//the main program simply initializes and displays the frame
